324e1be2c69b6fcf07043323a1d3e4c27b5cfb8d
HamzaYacub <hamzayacub97@gmail.com>
2021-11-08 16:18:57 +0000

    updated func again

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,13 @@
-def fibonacci():
-    n = input("Enter the number of terms you require ")
-    terms = int(n)
+n = input("Enter the number of terms you require ")
+terms = int(n)
 
-    n_0 = 0
-    n_1 = 1
-    n_2 = n_1 + n_0
-    fibo = [n_0, n_1, n_2]
+n_0 = 0
+n_1 = 1
+n_2 = n_1 + n_0
+fibo = [n_0, n_1, n_2]
     
-    for i in range(3,terms):
-        n_3 = fibo[-1] + fibo[-2]
-        fibo.append(n_3)
+for i in range(3,terms):
+    n_3 = fibo[-1] + fibo[-2]
+    fibo.append(n_3)
+
+print(fibo)
