ff227aade2a06e18c821651ef0f387b9fc4fa3f6
miguel8998 <miguel8998@hotmail.com>
2021-11-08 12:22:11 +0000

    add gitignore and edit the main file

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import abdu_func
 import hamza_func
 import pratik_func
 
-abdu_func.my_func
-hamza_func.my_func
-pratik_func.my_func
+abdu_func.my_func("Abdu")
+hamza_func.my_func()
+pratik_func.my_func()
