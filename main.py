56b5301e0e1f05682dd79e18b4079f7150f06a52
miguel8998 <miguel8998@hotmail.com>
2021-11-08 12:25:58 +0000

    change Hamza function to call the right name

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import abdu_func
 import hamza_func
-import pratik_func
+#import pratik_func
 
 abdu_func.my_func("Abdu")
-hamza_func.my_func("Hamza")
-pratik_func.my_func()
+hamza_func.my_name("Hamza")
+#pratik_func.my_func()
